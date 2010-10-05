c3bc3335182e4056a88f4831054f9cb4b229b937
tmy1018 <tmy1018@8cab289c-3b43-f1f8-8913-00ff348f46a9>
2010-10-05 11:44:10 +0000

    working on main part, parsing ain't easy

---
+++ eval_bool_expr.cpp
@@ -0,0 +1,76 @@
+/*
+ *  Copyright (C) 2010 Tianyang Li
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author Tianyang Li 
+ * E-Mail tmy1018 gmail com
+ */
+
+#include "eval_bool_expr.h"
+
+EvalBoolExpr::EvalBoolExpr(std::map<std::string, bool> const &var_map, 
+						   std::string const &expr_str)
+: expr_var_(&var_map), input_str_(&expr_str) {
+}
+
+bool EvalBoolExpr::InitBuildEvalStruct() {
+	EvalNode cur_expr(*expr_var_, *input_str_);
+	if (!cur_expr.InitSetExprLoc(0, input_str_->length() - 1)) {
+		return false;
+	}
+
+	expr_tree_.push_back(cur_expr);
+	return true;
+}
+
+bool EvalBoolExpr::EvalNode::InitSetExprLoc(unsigned int expr_begin, 
+											unsigned int expr_end) {
+	// TODO: strip away as many ( ) as possible
+	// TODO: set node_op_
+	return true;
+}
+
+bool EvalBoolExpr::EvalNode::DoEval() {
+	switch (node_op_) {
+		case OP_AND_:
+			return left_->DoEval() && right_->DoEval();
+			break;
+
+		case OP_OR_:
+			return left_->DoEval() || right_->DoEval();
+			break;
+
+		case OP_NOT_:
+			return !left_->DoEval();
+			break;
+
+		case OP_PAR_:
+			return left_->DoEval();
+			break;
+
+		case OP_VAR_:
+			return node_expr_var_->find(node_input_str_->substr(expr_loc_.first, 
+				expr_loc_.second - expr_loc_.first + 1))->second;
+			break;
+	}
+}
+
+EvalBoolExpr::EvalNode::EvalNode(std::map<std::string, bool> const &var_val,
+								 std::string const &expr_str) 
+: left_(0), right_(0), node_op_(OP_VAR_),
+node_expr_var_(&var_val), node_input_str_(&expr_str) {
+}
+
+
---
+++ eval_bool_expr.h
@@ -0,0 +1,84 @@
+/*
+ *  Copyright (C) 2010 Tianyang Li
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author Tianyang Li 
+ * E-Mail tmy1018 gmail com
+ */
+
+// the input expression is already 
+// processed by SimplifyBoolExpr::InitCheckInput()
+// input_str_ can't point to an empty string
+
+#ifndef QM4C_EVAL_BOOL_EXPR_H_
+#define QM4C_EVAL_BOOL_EXPR_H_
+
+#include <map>
+#include <string>
+#include <vector>
+#include <utility>
+
+class EvalBoolExpr {
+public:
+	EvalBoolExpr(std::map<std::string, bool> const &var_map, std::string const &expr_str);
+
+	// build structure for evaluation
+	// returns false if an error occurs
+	bool InitBuildEvalStruct();
+
+private:
+	// get each variable's value from here
+	std::map<std::string, bool> const *expr_var_;
+	std::string const *input_str_;
+
+	// not really a node in the tree, but it's like a tree stored in a vector
+	class EvalNode {
+		friend class EvalBoolExpr;
+
+	public:
+
+	private:
+		EvalNode(std::map<std::string, bool> const &var_val,
+			std::string const &expr_str);
+		// get evaluation result
+		bool DoEval();
+
+		// set location of the expression
+		// returns false if error is found
+		bool InitSetExprLoc(unsigned int expr_begin, unsigned int expr_end);
+
+		std::pair<unsigned int, unsigned int> expr_loc_;
+
+		// OP_VAR_ are for a single variable
+		// OP_NOT_, OP_PAR_ will only use left_
+		// OP_PAR_ is for something like (X)
+		// these 5 are listed in order of increasing precedence
+		enum NodeType_ {OP_OR_, OP_AND_, OP_NOT_, OP_PAR_, OP_VAR_};
+
+		// this is to be set in SetExprLoc()
+		NodeType_ node_op_;
+
+		EvalBoolExpr::EvalNode *left_, *right_;
+
+		std::map<std::string, bool> const *node_expr_var_;
+		std::string const *node_input_str_;
+	};
+
+	std::vector<EvalNode> expr_tree_;
+
+};
+
+#endif  // QM4C_EVAL_BOOL_EXPR_H_
+
