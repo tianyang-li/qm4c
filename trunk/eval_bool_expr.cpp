2cf612e0b6089ed63bbafb73a8d58e1f1bc6634c
tmy1018 <tmy1018@8cab289c-3b43-f1f8-8913-00ff348f46a9>
2010-10-05 14:13:04 +0000

    making some change to how i parse the expression

--- trunk/eval_bool_expr.cpp
+++ trunk/eval_bool_expr.cpp
@@ -25,6 +25,7 @@ EvalBoolExpr::EvalBoolExpr(std::map<std::string, bool> const &var_map,
 : expr_var_(&var_map), input_str_(&expr_str) {
 }
 
+/*
 bool EvalBoolExpr::InitBuildEvalStruct() {
 	EvalNode cur_expr(*expr_var_, *input_str_);
 	if (!cur_expr.InitSetExprLoc(0, input_str_->length() - 1)) {
@@ -37,8 +38,28 @@ bool EvalBoolExpr::InitBuildEvalStruct() {
 
 bool EvalBoolExpr::EvalNode::InitSetExprLoc(unsigned int expr_begin, 
 											unsigned int expr_end) {
-	// TODO: strip away as many ( ) as possible
-	// TODO: set node_op_
+	// stores each pair of parentheses
+	std::vector< std::pair<unsigned int, unsigned int> > temp_par_pair;
+
+	for (unsigned int i = expr_begin; i <= expr_end; ++i) {
+	}
+
+	// TODO: expr processing
+
+	EvalNode temp_eval_node(var;
+	switch (node_op_) {
+		case OP_VAR_:
+			break;
+		case OP_PAR_:
+			// check for empty parenthesis, sth like ()
+			if (expr_begin + 1 == expr_end) {
+				return false;
+			}
+			temp_eval_node
+
+			break;
+	}
+	
 	return true;
 }
 
@@ -72,5 +93,6 @@ EvalBoolExpr::EvalNode::EvalNode(std::map<std::string, bool> const &var_val,
 : left_(0), right_(0), node_op_(OP_VAR_),
 node_expr_var_(&var_val), node_input_str_(&expr_str) {
 }
+*/
 
 
